c2fc4bd444590bbd5d7f5ea6e1ac4a28cc3d29c3
Andres Coit <andrescoit@gmail.com>
2019-02-27 17:37:20 -0600

    Added existing code

---
+++ ocr-python/README.md
@@ -0,0 +1,3 @@
+Make sure to download all the necessary libraries: Opencv, numpy, pytesseract, and PIL
+
+Code is likely not refined enough for final version, as it has trouble with real-world pictures; but it should serve as a good starting point for the OCR portion of the robot.
---
+++ ocr-python/ocr.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
+import sys
+import os.path
+
+# usage:
+# $python ocr.py [filename.jpg]
+
+input_file = sys.argv[1]
+
+def get_string(img_path):
+    # Read image with opencv
+    img = cv2.imread(img_path)
+
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Write image after removed noise
+    cv2.imwrite("removed_noise.png", img)
+
+    # Apply threshold to get image with only black and white
+    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+
+    # Write the image after apply opencv to do some ...
+    cv2.imwrite(img_path, img)
+
+    # Recognize text with tesseract for python
+    result = pytesseract.image_to_string(Image.open(img_path))
+
+    return result
+
+
+# Remove in final version
+# Instead call get_string function from another file
+print(get_string(input_file))
+
+
+
+
+
