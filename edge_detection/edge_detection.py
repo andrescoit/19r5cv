814208d2443fd9addb0ce41e0f1344ab217707ca
Andres Coit <andrescoit@gmail.com>
2019-03-15 19:11:35 +0000

    edge detection and ocr modified to work live

--- edge_detection/edge_detection.py
+++ edge_detection/edge_detection.py
@@ -1,20 +1,36 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
+import imutils
 
+#width = 800
 
 def canny_webcam():
     "Live capture frames from webcam and show the canny edge image of the captured frames."
 
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(0)
+    time.sleep(0.5) # allow comera warmup
 
     while True:
-        ret, frame = cap.read()  # ret gets a boolean value. True if reading is successful (I think). frame is an
+        #ret, frame = cap.read()  # ret gets a boolean value. True if reading is successful (I think). frame is an
+        (ret, frame) = camera.read()
         # uint8 numpy.ndarray
 
-        frame = cv2.GaussianBlur(frame, (7, 7), 1.41)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #frame = imutils.resize(frame, width=width)
 
-        edge = cv2.Canny(frame, 25, 75)
+        #frame = cv2.GaussianBlur(frame, (7, 7), 1.41)
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if ret is True:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        else:
+            break
+        gray = cv2.GaussianBlur(gray, (7,7), 1.41)
 
+        #edge = cv2.Canny(frame, 25, 75)
+        edge = cv2.Canny(gray, 25, 75)
+ 
         cv2.imshow('Canny Edge', edge)
 
         if cv2.waitKey(20) == ord('q'):  # Introduce 20 milisecond delay. press q to exit.
--- ocr-python/ocr.py
+++ ocr-python/ocr.py
@@ -4,43 +4,74 @@ import pytesseract
 from PIL import Image
 import sys
 import os.path
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
 # usage:
 # $python ocr.py [filename.jpg]
 
-input_file = sys.argv[1]
+#input_file = sys.argv[1]
 
-def get_string(img_path):
+
+#def get_string(img_path):
+def get_string():
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 5
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    
     # Read image with opencv
-    img = cv2.imread(img_path)
+    #img = cv2.imread(img_path)
+    
+    time.sleep(0.5)
 
-    # Convert to gray
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        img = frame.array
+        # Convert to gray
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Apply dilation and erosion to remove some noise
-    kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
+        # Apply dilation and erosion to remove some noise
+        kernel = np.ones((1, 1), np.uint8)
+        img = cv2.dilate(img, kernel, iterations=1)
+        img = cv2.erode(img, kernel, iterations=1)
 
-    # Write image after removed noise
-    cv2.imwrite("removed_noise.png", img)
+        # https://github.com/tesseract-ocr/tesseract/wiki/Command-Line-Usage
+        # --psm 10 "Treat the image as a single character."
+        # --oem  2 "Legacy + LSTM engines"
+        tessdata_dir_config = '--tessdata-dir "/usr/share/tesseract-ocr/tessdata" --psm 10 --oem 2 -c tessedit_char_whitelist=ABCDEFG'
 
-    # Apply threshold to get image with only black and white
-    #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+        # Write image after removed noise
+        #cv2.imwrite("removed_noise.png", img)
 
-    # Write the image after apply opencv to do some ...
-    cv2.imwrite(img_path, img)
+        # Apply threshold to get image with only black and white
+        #img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
-    # Recognize text with tesseract for python
-    result = pytesseract.image_to_string(Image.open(img_path))
+        # Write the image after apply opencv to do some ...
+        #cv2.imwrite(img_path, img)
 
-    return result
+        # Prepare for next frame
+        rawCapture.truncate(0)
+
+        # Recognize text with tesseract for python
+        #result = pytesseract.image_to_string(Image.open(img_path))
+        result = pytesseract.image_to_string(img, config = tessdata_dir_config)
+        print result
+        print "\n"
+
+        # Show corresponding image
+        cv2.imshow('OCR View', img) 
+        if cv2.waitKey(200) == ord('q'):
+            break
+
+    return 0
+    #return result
 
 
 # Remove in final version
 # Instead call get_string function from another file
-print(get_string(input_file))
-
+#print(get_string(input_file))
+get_string()
 
 
 
